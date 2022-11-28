6ac1c24fb0b03fb73ad14e56d65f57441ec8b978
MuhammadIbrahimAlvi <ebrahimalvi2@gmail.com>
2022-11-29 00:59:06 +0500

    origin/hackerrank Reverse List problem.

--- hackerrankdsa/ReverseArray.java
+++ hackerrankdsa/ReverseArray.java
@@ -7,7 +7,7 @@ public class ReverseArray {
     public static void main(String[] args) {
         ReverseArrayImpl reverseArray = new ReverseArrayImpl();
         List<Integer> integerList = new ArrayList<>();
-        for (int i = 0; i<5;i++){
+        for (int i = 0; i < 5; i++) {
             integerList.add(i);
         }
         System.out.println(reverseArray.reverseArray(integerList));
