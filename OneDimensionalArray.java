7b7ae1ebe4e8856715963bc10ff361b8f48773ea
MuhammadIbrahimAlvi <ebrahimalvi2@gmail.com>
2022-11-29 00:54:55 +0500

    origin/hackerrank Reverse List problem.

--- OneDimensionalArray.java
+++ OneDimensionalArray.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class OneDimensionalArray {
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+
+    }
 }
--- hackerrankdsa/ReverseArray.java
+++ hackerrankdsa/ReverseArray.java
@@ -1,2 +1,16 @@
-package hackerrankdsa;public class ReverseArray {
+package hackerrankdsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        ReverseArrayImpl reverseArray = new ReverseArrayImpl();
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i<5;i++){
+            integerList.add(i);
+        }
+        System.out.println(reverseArray.reverseArray(integerList));
+    }
 }
+
