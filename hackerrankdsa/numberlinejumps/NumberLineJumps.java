00dc5faf4188a92a6ae5bd286acef628209f1fc4
MuhammadIbrahimAlvi <ebrahimalvi2@gmail.com>
2022-11-29 01:09:38 +0500

    origin/hackerrank NumberLineJumps problem.

---
+++ hackerrankdsa/numberlinejumps/NumberLineJumps.java
@@ -0,0 +1,8 @@
+package hackerrankdsa.numberlinejumps;
+
+public class NumberLineJumps {
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        return "";
+    }
+}
