c4dda4066c1f2388b4272ff0bc4dfa39d7083b93
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-10-01 02:18:38 +0500

    main Added multiple problem classes.

--- problemsolving/src/leetcode/OneThreeTwoPattern.java
+++ problemsolving/src/leetcode/OneThreeTwoPattern.java
@@ -3,6 +3,16 @@ package leetcode;
 /**
  * Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
  * Return true if there is a 132 pattern in nums, otherwise, return false.
- * **/
+ **/
+
 public class OneThreeTwoPattern {
+    public static void main(String[] args) {
+        int[] arrayOfNumbers = {1, 3, 2, 4};
+        System.out.println(find132pattern(arrayOfNumbers));
+    }
+
+    public static boolean find132pattern(int[] nums) {
+        return true;
+    }
+
 }
