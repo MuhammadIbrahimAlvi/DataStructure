6d63c19c89590d9ad2b1eb8c4aa1f74d54fa0719
Muhammad Ibrahim Alvi <ebrahimalvi2@gmail.com>
2023-10-04 23:29:57 +0500

    main Palindrome checking program if the word (String type) is palindrome or not.

--- problemsolving/src/problemsolving/CheckPalindrome.java
+++ problemsolving/src/problemsolving/CheckPalindrome.java
@@ -5,4 +5,18 @@ package problemsolving;
  * **/
 
 public class CheckPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Madam"));
+    }
+
+    private static boolean isPalindrome(String word){
+        String temporaryWord = "";
+        for (int i = word.length()-1; i>=0 ; i--){
+            temporaryWord += word.charAt(i);
+        }
+        if(temporaryWord.equalsIgnoreCase(word)){
+            return true;
+        }
+        return false;
+    }
 }
